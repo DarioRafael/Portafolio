9f05e11a3364f59b47af9d669c1da3c486130263
Kawa <dariorafa29@hotmail.com>
2024-07-15 13:44:46 -0600

    Mejorando cosas del portafolio: - Aprendiendo a agregar idiomas diferentes. - Conociendo tipos de funciones. - Se añade el boton de idiomas - Se esta trabajando para optimizar el codigo

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
     const menuToggle = document.querySelector(".menu-toggle");
     const navMenu = document.querySelector(".nav-menu");
+    const languageToggle = document.querySelector(".language-toggle");
+    const languageMenu = document.querySelector(".language-menu");
 
     menuToggle.addEventListener("click", function() {
+        if (languageMenu.classList.contains("active")) {
+            languageMenu.classList.remove("active");
+        }
         navMenu.classList.toggle("active");
     });
 
-    window.addEventListener("resize", function() {
-        if (window.innerWidth > 768 && navMenu.classList.contains("active")) {
+    languageToggle.addEventListener("click", function() {
+        if (navMenu.classList.contains("active")) {
             navMenu.classList.remove("active");
+        }
+        languageMenu.classList.toggle("active");
+    });
+
+    window.addEventListener("resize", function() {
+        if (window.innerWidth > 768) {
+            if (navMenu.classList.contains("active")) {
+                navMenu.classList.remove("active");
+            }
+            if (languageMenu.classList.contains("active")) {
+                languageMenu.classList.remove("active");
+            }
         }
         if (window.innerWidth <= 768) {
             navMenu.classList.add("no-transition");
@@ -18,3 +35,19 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    changeLanguage('es'); // Idioma por defecto
+});
+
+function changeLanguage(language) {
+    fetch('translations.json')
+        .then(response => response.json())
+        .then(translations => {
+            document.querySelectorAll('[data-key]').forEach(element => {
+                const key = element.getAttribute('data-key');
+                element.textContent = translations[language][key];
+            });
+            document.title = translations[language]['site_title'];
+        });
+}
